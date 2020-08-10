05b0301890b0b2c18a56680e21eb99eaa782eb68
86183 <2903740155@qq.com>
2020-08-10 14:09:00 +0800

    2020-8-10 第一次搭建SpringBoot框架
    Signed-off-by: 86183 <2903740155@qq.com>

---
+++ src/main/java/com/hqyj/javaSpringBoot/modules/test/controller/TestSpringBootController.java
@@ -0,0 +1,20 @@
+package com.hqyj.javaSpringBoot.modules.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/test")
+public class TestSpringBootController {
+
+    /**
+     * 127.0.0.1:8080/test/testOne
+     */
+    @GetMapping("/testOne")
+    @ResponseBody
+    public String testOne(){
+        return "Test SpringBoot Successfully!";
+    }
+}
---
+++ src/test/java/com/hqyj/javaSpringBoot/JavaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hqyj.javaSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
