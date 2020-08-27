438dc7f9511399b9e761a3216b0b8bec1bab4acc
86183 <2903740155@qq.com>
2020-08-27 09:04:13 +0800

    Revert ""二次修改 user表的删除、修改、添加""
    Signed-off-by: 86183 <2903740155@qq.com>

--- src/main/java/com/hqyj/javaSpringBoot/JavaSpringBootApplication.java
+++ src/main/java/com/hqyj/javaSpringBoot/JavaSpringBootApplication.java
@@ -1,6 +1,4 @@
 package com.hqyj.javaSpringBoot;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/hqyj/javaSpringBoot/modules/account/dao/UserRoleDao.java
+++ src/main/java/com/hqyj/javaSpringBoot/modules/account/dao/UserRoleDao.java
@@ -1,9 +1,7 @@
 package com.hqyj.javaSpringBoot.modules.account.dao;
 
 import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -11,7 +9,4 @@ import org.springframework.stereotype.Repository;
 public interface UserRoleDao {
     @Delete("delete from user_role where user_id=#{userId}")
     void deleteUserRoleByUserId(int userId);
-
-    @Insert("insert user_role(role_id, user_id) value(#{roleId}, #{userId})")
-    void addUserRole(@Param("userId") int userId, @Param("roleId") int roleId);
 }
